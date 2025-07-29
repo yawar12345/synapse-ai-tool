b005a92f6eb61a246d9ce43d914645fda6911be2
yawar12345 <40006106+yawar12345@users.noreply.github.com>
2025-07-29 15:56:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -305,4 +305,5 @@ function generateJmxFromRequests(requests) {
 app.listen(port, () => {
     console.log(`\n🚀 Synapse AI Dashboard server is running.`);
     console.log(`   Access the UI at http://localhost:${port}`);
+
 });
